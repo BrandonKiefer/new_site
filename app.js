1281316f2d5658e1708687619bf1c9aab049a07c
kittenmasque <volaju@gmail.com>
2022-06-08 03:39:12 -0700

    french translation for myBikeDatabase

--- app.js
+++ app.js
@@ -63,65 +63,6 @@ $(document).ready(function () {
       window.addEventListener("scroll", revealsideL);
 
 
-    $(`.javascript`).on("click", function () {
-        if (toggleActive = true) {
-            $(`#css`).hide()
-            $(`#html_`).hide()
-            $(`#photoshop`).hide()
-            $(`#french`).hide()
-            toggleActive = false
-        }
-        $(`#javascript`).show();
-        toggleActive = true;
-    })
-
-    $(`.css`).on("click", function () {
-        if (toggleActive = true) {
-            $(`#javascript`).hide()
-            $(`#html_`).hide()
-            $(`#photoshop`).hide()
-            $(`#french`).hide()
-            toggleActive = false
-        }
-        $(`#css`).show();
-        toggleActive = true;
-    })
-
-    $(`.html_`).on("click", function () {
-        if (toggleActive = true) {
-            $(`#javascript`).hide()
-            $(`#css`).hide()
-            $(`#photoshop`).hide()
-            $(`#french`).hide()
-            toggleActive = false
-        }
-        $(`#html_`).show();
-        toggleActive = true;
-    })
-
-    $(`.photoshop`).on("click", function () {
-        if (toggleActive = true) {
-            $(`#javascript`).hide()
-            $(`#html_`).hide()
-            $(`#css`).hide()
-            $(`#french`).hide()
-            toggleActive = false
-        }
-        $(`#photoshop`).show();
-        toggleActive = true;
-    })
-
-    $(`.french`).on("click", function () {
-        if (toggleActive = true) {
-            $(`#javascript`).hide()
-            $(`#html_`).hide()
-            $(`#photoshop`).hide()
-            $(`#css`).hide()
-            toggleActive = false
-        }
-        $(`#french`).show();
-        toggleActive = true;
-    })
 
     const rufus = document.getElementsByClassName("rufus")
     const chatBubble = document.getElementsByClassName(`chat_bubble`);
@@ -137,11 +78,20 @@ $(document).ready(function () {
     $(FR).click(function(){
     french = true;
     if (french === true){
-    $(`p.myBikeDatabase`).text(`I suis né á Mobile, AL (qui était la première colonie de la Louisiane-Française avant être acheté par les États-Unis en 1803.) 
-    J'étudias á l'Université de South Alabama où je me suis specialisé en deux sujets-- le français et l'art. 
-    En 2012, j'avais la chance étudier á l'étranger á l'Université Paul Valery chez Montpellier, France. 
-    Après avoir obtenu mon diplôme, j'étais permi dans le programme TAPIF pour être un assistant d'enseigner l'anglais.  
-    Lorsqu'il s'est terminé, je me suis rendu aux États-Unis et finalement me trouvé au San Francisco Bay Area où j'habite hereusement depuis ce-temps lá.`);
+    $(`p.myBikeDatabase`).replaceWith(`<p class="myBikeDatabase reveal">MyBikeDatabase est une application qui permet l'utilisateur à suivre les spécifications de nombreux de ses vélos. 
+    Il peut-être utilise pour enregrister des spécifications très obscur lequel peut-être très facile oublier mais très difficile mesurer-- pourquoi le faire plus qui soit nécessaire? 
+    Aussi, il possède la mise en pratique commerciale pour les magasins du vélo à gerer leurs clients. Comme tous les applications full-CRUD, les vélos soumis ont la capacité être modifié et supprimé par le créateur. 
+    Quelques des technologies utilisé incluent:
+    <br/>
+    <br/>
+        • MongoDB</br>
+        • React</br>
+        • Express</br>
+        • Node</br>
+        • Javascript</br>
+    <br/>
+    A l'instant, myBikeDatabase est hébergé sur Heroku et il y aura des additions dans l'avenir qui incluent à se connecter et s'inscrire.
+</p>`);
     $(rufus).attr("src", "images/rufus_francais.png")
     $(chatBubble).text("Coucou mon pôte, est-ce que vous avez besoin d'assistance pour naviguer mon site?")
     $(`#javascript`).text(`J'ai programmé des jeux en text`)
@@ -152,10 +102,16 @@ $(document).ready(function () {
     $(`#projets`).text(`Projets`)
     $(`#experience`).text(`Expérience`)
     $(`#skill`).text(`Competences`)
-    $(`#about`).text(`Je suis un ingénieur-conseil en informatique qui possede des experiences en Javascript, React, HTML5, jQuery, CSS, 
-    et des environnement dorsal en utilisant le Python, Django, Express, REST APIs, MongoDB, et SQL. 
+    $(`#about`).replaceWith(`<p id="about">Je suis un ingénieur-conseil en informatique qui possede des experiences en Javascript, React, HTML5, jQuery, CSS, et des environnement dorsal en utilisant le Python, Django, Express, REST APIs, MongoDB, et SQL. 
+    <br/>
+    <br/>
     Avant d'être devenu un ingénieur-conseil, je me specialisais aux beaux-arts et le français-- ceux les deux continuent me soutenier comme je les intègre à ma vie du coding. 
-    J'espère me trouver en travaillant pour une entreprise qui partage mes valeurs en paix, le traitement respecteux aux animaux, et préserver le climat habitable de notre monde.`)
+    <br/>
+    <br/>
+    J'espère me trouver en travaillant pour une entreprise qui partage mes valeurs en paix, le traitement respecteux aux animaux, et préserver le climat habitable de notre monde.
+    <br/>
+    <br/>
+   <a id="resume" target="_blank" href="/POST-BOOTCAMP/new_site/resume_web.pdf">Cliquez ici</a></p>`)
     $(`#resume`).replaceWith('<p id="resume">Pour voir mon CV, <a href="resume.pdf" target="_blank"><u>cliquer ici</u></a>.</p>')
     $(`#terminal`).text("Des jeux en Terminale")
     }    
@@ -165,22 +121,34 @@ $(document).ready(function () {
 const EN = document.getElementsByClassName("en")
 $(EN).click(function(){
 french = false;
-$(`#about`).text(`I'm a software engineer with experience in Javascript, React, HTML5, jQuery, CSS, and backend frameworks using Python, Django, Express, REST APIs, MongoDB, and SQL. 
+$(`#about`).replaceWith(`<p id="about">I'm a software engineer with experience in Javascript, React, HTML5, jQuery, CSS, and backend frameworks using Python, Django, Express, REST APIs, MongoDB, and SQL. 
+<br/>
+<br/>
 Prior to becoming a software engineer, my background was in Art and French which has been fun to incorporate into my coding life. 
-That said, I hope to lend my skills and offer creative solutions for companies that share my values in peace, treating animals with respect, and preserving our planet's habitable climate.`);
-$(rufus).attr("src", "images/rufus_closeup2.png")
-$(chatBubble).text("Hey buddy, it looks like you want to use my website. Need any help?")
-$(`#javascript`).text(`So far in Javascript I've made some terminal games.`)
-$(`#css`).text(`This is mostly what I've used to give style to this website. Pretty handy!`)
-$(`#html_`).text(`Super useful for building websites!`)
-$(`#photoshop`).text(`I've been using Photoshop for over 20 years. I took 2 semesters of it in high school and even had a photo editor position before coming to California.`)
-$(`#french`).text(`Communication can sometimes make all the difference. In addition to computer languages-- I can also speak, read, and write in French!`)
+<br/>
+<br/>
+That said, I hope to lend my skills and offer creative solutions for companies that share my values in peace, treating animals with respect, and preserving our planet's habitable climate.
+<br/>
+<br/>
+<a id="resume" target="_blank" href="/POST-BOOTCAMP/new_site/resume_web.pdf">Click here</a></p>`);
+$(`p.myBikeDatabase`).replaceWith(`<p class="myBikeDatabase reveal">MyBikeDatabase is a full-stack MERN application that allows users to keep track of the metrics for their bicycles' components using a very thorough and detailed bike schema.
+It can be used for personal record-keeping to ensure compatibility for replacing and ordering new parts. It also has commercial application for client/inventory management.Like any full-CRUD application, submitted bicycles can be edited and deleted.
+Some of the coding technologies used include:
+<br/>
+<br/>
+    • MongoDB</br>
+    • React</br>
+    • Express</br>
+    • Node</br>
+    • Javascript</br>
+<br/>
+Currently hosted on Heroku, updates and plans for future deployment include a Registration and Login component for users.
+</p>`)
 $(`#projets`).text(`Projects`)
 $(`#skill`).text(`Skills`)
 $(`#welcome`).text(`Welcome to my website :)`)
 $(`#experience`).text(`Experience`)
 $(`#resume`).replaceWith('<p id="resume">To view my resume, <a href="resume.pdf" target="_blank"><u>click here</u></a>.</p>')
-$(`#terminal`).text("Terminal Games")
 })
 
 
