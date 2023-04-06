f339c3db1a53d6876d284c951ed882c4754a44c5
kittenmasque <volaju@gmail.com>
2023-04-05 18:01:13 -0700

    refactored old site while keeping nav and footer with styles and implented image carousel with fading css animation

--- app.js
+++ app.js
@@ -1,78 +1,78 @@
-$(document).ready(function () {
+// Keep track of the index of the currently displayed image
+let currentImageIndex = 0;
+
+// Get all image elements inside the container div
+const images = document.querySelectorAll('.carousel img');
+
+// Get the total number of images
+const imageCount = images.length;
+
+const dots = document.querySelectorAll('.dot');
 
 
+// Set up an interval to switch images every 10 seconds
+setInterval(() => {
+  // Hide the currently displayed image
+  images[currentImageIndex].classList.remove('active');
 
-  function reveal() {
-    let reveals = document.querySelectorAll(".reveal")
-    for (let i = 0; i < reveals.length; i++) {
-      //window.innerHeight will give us the height of the viewport
-      let windowHeight = window.innerHeight;
-      //getBoundingClientRect().top gives us this distance from the top of the viewport 
-      let elementTop = reveals[i].getBoundingClientRect().top;
-      //elementVisible is the height at which the element should be revealed to the user.
-      let elementVisible = 125;
+  // Increment the index of the currently displayed image,
+  // wrapping back to the beginning if necessary
+  currentImageIndex = (currentImageIndex + 1) % imageCount;
 
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
-  }
-
-  function revealsideR() {
-    let reveals = document.querySelectorAll(".reveal-side-r")
-    for (let i = 0; i < reveals.length; i++) {
-      //window.innerHeight will give us the height of the viewport
-      let windowHeight = window.innerHeight;
-      //getBoundingClientRect().top gives us this distance from the top of the viewport 
-      let elementTop = reveals[i].getBoundingClientRect().top;
-      //elementVisible is the height at which the element should be revealed to the user.
-      let elementVisible = 150;
-
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
-  }
-
-  function revealsideL() {
-    let reveals = document.querySelectorAll(".reveal-side-l")
-    for (let i = 0; i < reveals.length; i++) {
-      //window.innerHeight will give us the height of the viewport
-      let windowHeight = window.innerHeight;
-      //getBoundingClientRect().top gives us this distance from the top of the viewport 
-      let elementTop = reveals[i].getBoundingClientRect().top;
-      //elementVisible is the height at which the element should be revealed to the user.
-      let elementVisible = 150;
-
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
-  }
+  // Show the newly displayed image
+  images[currentImageIndex].classList.add('active');
+}, 10000);
 
 
+// Add click event listeners to the dots
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    // Return if the clicked dot is already active
+    if (index === currentImageIndex) return;
 
-  window.addEventListener("scroll", reveal);
-  window.addEventListener("scroll", revealsideR);
-  window.addEventListener("scroll", revealsideL);
+    // Deactivate the currently active image and dot
+    images[currentImageIndex].classList.remove('active');
+    dots[currentImageIndex].classList.remove('active');
 
+    // Set the index of the newly displayed image
+    currentImageIndex = index;
 
+    // Activate the newly displayed image and dot
+    images[currentImageIndex].classList.add('active');
+    dots[currentImageIndex].classList.add('active');
+  });
+});
 
-  const rufus = document.getElementsByClassName("rufus")
-  const chatBubble = document.getElementsByClassName(`chat_bubble`);
+// Add event listener for left arrow click
+document.querySelector('.left-arrow').addEventListener('click', () => {
+  moveImage(currentImageIndex - 1);
+});
 
-  $(rufus).click(function () {
-    $(chatBubble).text(`Ah, I forgot I am a dog. Well, good luck buddy.`);
-    if (french === true) {
-      $(chatBubble).text(`Ah, j'oublie que je suis un chien. Bon courage mon pôte.`);
-    }
-  })
+// Add event listener for right arrow click
+document.querySelector('.right-arrow').addEventListener('click', () => {
+  moveImage(currentImageIndex + 1);
+});
+
+function moveImage(newIndex) {
+  // Adjust the new index if it's out of bounds
+  if (newIndex < 0) newIndex = imageCount - 1;
+  if (newIndex >= imageCount) newIndex = 0;
+
+  // Deactivate the currently active image and dot
+  images[currentImageIndex].classList.remove('active');
+  dots[currentImageIndex].classList.remove('active');
+
+  // Set the index of the newly displayed image
+  currentImageIndex = newIndex;
+
+  // Activate the newly displayed image and dot
+  images[currentImageIndex].classList.add('active');
+  dots[currentImageIndex].classList.add('active');
+}
+
+// Set the first image and its corresponding dot as active on page load
+images[0].classList.add('active');
+dots[0].classList.add('active');
 
   const FR = document.getElementsByClassName("fr")
   $(FR).click(function () {
@@ -203,4 +203,4 @@ original duration of the show, and any remarkable/historically significant notes
 
 
 
-})
+
